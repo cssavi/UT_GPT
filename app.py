92ad2f271f9cb3da02dbcf18c6e67dbda9dcf08e
Savagedu <savinduw@gmail.com>
2023-07-29 00:20:13 -0400

    Added index.html file inside new templates folder as well as an app.py file to run the flask backend and call functions in the original utgpt.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from utgpt import setup_pinecone, query_openai, query_with_sources, os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = ""
+    if request.method == 'POST':
+        query = request.form.get('query')
+        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+        vectorstore = setup_pinecone(OPENAI_API_KEY)
+        result = query_openai(vectorstore, OPENAI_API_KEY, query)
+    return render_template('index.html', result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
