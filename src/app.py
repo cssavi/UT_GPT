bd288dc26fe8c6e9daa2c8050e06a6b993970473
Savagedu <savinduw@gmail.com>
2023-07-30 17:53:04 -0400

    Added an agent, error handling, & upped efficiency

--- src/app.py
+++ src/app.py
@@ -1,18 +1,28 @@
 import os
+import logging
 from flask import Flask, render_template, request
-from utgpt import setup_pinecone, query_openai, query_with_sources
+from utgpt import setup_pinecone, initialize_openai, query_with_sources
+from dotenv import load_dotenv
+
 
 template_dir = os.path.abspath('templates')
 app = Flask(__name__, template_folder=template_dir)
 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+vectorstore = setup_pinecone(OPENAI_API_KEY)
+agent = initialize_openai(vectorstore, OPENAI_API_KEY)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     result = ""
-    if request.method == 'POST':
-        query = request.form.get('query')
-        OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-        vectorstore = setup_pinecone(OPENAI_API_KEY)
-        result = query_openai(vectorstore, OPENAI_API_KEY, query)
+    try:
+        if request.method == 'POST':
+            query = request.form.get('query')
+            result = agent(query)['output']
+    except Exception as e:
+        logging.error(f"An error occurred while processing the query: {str(e)}")
+        result = "An error occurred while processing your query. Please try again later."
     return render_template('index.html', result=result)
 
 if __name__ == "__main__":
--- src/utgpt.py
+++ src/utgpt.py
@@ -3,8 +3,11 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.embeddings.openai import OpenAIEmbeddings
-import pinecone
+from langchain.agents import Tool
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.agents import initialize_agent
 from dotenv import load_dotenv
+import pinecone
 import os
 
 
@@ -24,11 +27,21 @@ def setup_pinecone(OPENAI_API_KEY):
     vectorstore = Pinecone(index, embed.embed_query, text_field)
     return vectorstore
 
-# Function to query from OpenAI using Langchain
-def query_openai(vectorstore, OPENAI_API_KEY, query):
+# Function to initialize the agent to be queried from
+def initialize_openai(vectorstore, OPENAI_API_KEY):
     llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo', temperature=0.0)
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever())
-    return qa.run(query)
+    tools = [ Tool( name='Knowledge Base', func=qa.run, description=( 'use this tool for every query to get '
+                    'more information and stories on the topic'))]
+    conversational_memory = ConversationBufferWindowMemory( memory_key='chat_history', k=5, return_messages=True)
+    agent = initialize_agent( agent='chat-conversational-react-description', tools=tools, llm=llm, verbose=True,
+                            max_iterations=3, early_stopping_method='generate', memory=conversational_memory)
+    sys_msg = """You are a well-informed guide of the University of Texas at Austin. You are an expert in all '
+               ' things related to helping students who are attending UT Austin. Answer the user's questions 
+               with kind, succinct, and informative tone."""
+    prompt = agent.agent.create_prompt( system_message=sys_msg, tools=tools)
+    agent.agent.llm_chain.prompt = prompt
+    return agent
 
 # Function to query with sources too
 def query_with_sources(vectorstore, OPENAI_API_KEY, query):
@@ -36,16 +49,14 @@ def query_with_sources(vectorstore, OPENAI_API_KEY, query):
     qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever())
     return qa_with_sources(query)
 
-# Main function to run the script
+# Main function to run the script for testing
 def main():
     load_dotenv()
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     vectorstore = setup_pinecone(OPENAI_API_KEY)
     query = input("Input query: ")
-    result = query_openai(vectorstore, OPENAI_API_KEY, query)
+    result = initialize_openai(vectorstore, OPENAI_API_KEY, query)
     print(result)
-    #result_with_sources = query_with_sources(vectorstore, OPENAI_API_KEY, query)
-    #print(result_with_sources)
 
 # Run the main function when the script is executed directly
 if __name__ == "__main__":
